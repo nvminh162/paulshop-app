a1fba05f2d050b2fdefad9f1d5892f17f5fbb324
nvminh162 <nvminh162@gmail.com>
2025-06-22 13:43:58 +0700

    first commit

---
+++ src/main/java/com/nvminh162/laptopshop/LaptopshopFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopshopFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopshopFullstackApplication.class, args);
+    }
+
+}
